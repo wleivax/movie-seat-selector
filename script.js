a433d51e932abe339c09f25a9d42ae055e3e2d8f
wleivax <leivawalt@gmail.com>
2021-09-26 01:46:47 -0400

    local storage

--- script.js
+++ script.js
@@ -4,13 +4,64 @@ const count = document.getElementById('count');
 const  total = document.getElementById('total');
 const  movieSelect = document.getElementById('movie');
 
-const ticketPrice = +movieSelect.value;
+// Populate UI
+populateUI();
 
-// console.log(typeof ticketPrice);
+// Had to change CONST to LET because const was giving error when changing movie selector
+let ticketPrice = +movieSelect.value;
+
+// Save selected movie index and price
+
+function setMovieData(movieIndex,  moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
 
 // Update total and count
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
 
+    // copy selected seats into array
+    // map through array
+    // retun a new array indexes
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
 
+    localStorage.setItem('selectedSeats',JSON.stringify(seatsIndex));
+
+    const selectedSeatsCount = selectedSeats.length;
+
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+// get data from local storage and populate UI
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats')
+    );
+
+    if (selectedSeats!== null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if (selectedSeats.indexOf(index) > -1) {
+                seat.classList.add('selected');
+            }
+        });
+    }
+
+    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+    if (selectedMovieIndex !== null) {
+        movieSelect.selectedIndex = selectedMovieIndex
+    }
+}
+
+// Movie select event
+movieSelect.addEventListener('change',e  => {
+    ticketPrice = e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
+    updateSelectedCount();
+});
+
+// Seat click event
 container.addEventListener('click', e => {
     if (
         e.target.classList.contains('seat') && !e.target.classList.contains('occupied')
@@ -21,3 +72,5 @@ container.addEventListener('click', e => {
     }
 });
 
+// Initial count and total set
+updateSelectedCount();
